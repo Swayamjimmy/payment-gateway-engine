fdcd9af6d5cc367d1c1335a960d0546f830fe927
swayam <swayamwr@gmail.com>
2026-06-13 22:37:09 +0530

    implement getNode method for hashmap lookup

--- src/tree.cpp
+++ src/tree.cpp
@@ -34,3 +34,36 @@ void PaymentTree::addNode(const std::string& name, const std::string& parentName
     parent->children.push_back(newNode);
     nodeMap[name] = newNode;
 }
+
+// Look up any node by name in O(1) using the hash map
+TreeNode* PaymentTree::getNode(const std::string& name) {
+    auto it = nodeMap.find(name);
+    if (it != nodeMap.end()) {
+        return it->second;
+    }
+    return nullptr;
+}
+
+// Stub implementations - will be completed in the next step
+bool PaymentTree::lock(const std::string& /*nodeName*/, int /*userId*/) {
+    return false;
+}
+
+bool PaymentTree::unlock(const std::string& /*nodeName*/, int /*userId*/) {
+    return false;
+}
+
+bool PaymentTree::upgradeLock(const std::string& /*nodeName*/, int /*userId*/) {
+    return false;
+}
+
+std::string PaymentTree::toJson() const {
+    return "{}";
+}
+
+void PaymentTree::informAncestors(TreeNode* /*node*/, int /*delta*/) {}
+
+void PaymentTree::informDescendants(TreeNode* /*node*/, int /*delta*/) {}
+
+void PaymentTree::collectLockedDescendants(TreeNode* /*node*/, int /*userId*/,
+    std::vector<TreeNode*>& /*result*/, bool& /*valid*/) {}
